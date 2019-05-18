f03953dc3319017cadc22a200c049f0bd87c2316
dastagg(opus3) <dastagg@stagglinux.com>
2019-05-18 10:27:53 -0400

    Finished Bite 2

--- 2/regex.py
+++ 2/regex.py
@@ -11,7 +11,10 @@ def extract_course_times():
         "Challenge 2 Lectures 27:48"
         "Afterword 1 Lecture 05:02"
     )
-    pass
+    pattern = r"\d{2}:\d{2}"
+    course_times = re.findall(pattern, flask_course)
+
+    return course_times
 
 
 def get_all_hashtags_and_links():
@@ -22,10 +25,32 @@ def get_all_hashtags_and_links():
         "for Repeated API Calls - http://pybit.es/requests-cache.html "
         "#python #APIs"
     )
-    pass
+
+    pattern = "http"
+    text = tweet
+
+    match = re.search(pattern, text)
+
+    s = match.start()
+
+    tweet_match = tweet[s : len(tweet)]
+
+    txt1 = tweet_match.split("\b")
+    txt2 = [line.split() for line in txt1]
+    new_list = []
+    for i in range(0, len(txt2)):
+        l1 = txt2[i]
+        for w in l1:
+            new_list.append(w)
+    return new_list
 
 
 def match_first_paragraph():
     """Write a regular expression that returns  'pybites != greedy' """
     html = "<p>pybites != greedy</p>" "<p>not the same can be said REgarding ...</p>"
-    pass
+
+    pattern = "<p>(.+?)</p>"
+    text = html
+
+    match = re.findall(pattern, text)
+    return match[0]
