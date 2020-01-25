6262c8c47227804da153df81e8d906a03a27bb0a
dastagg(opus3) <dastagg@stagglinux.com>
2020-01-25 08:30:24 -0500

    Modified to pass tests on PyBytes

--- 46/fizzbuzz.py
+++ 46/fizzbuzz.py
@@ -1,6 +1,6 @@
 def fizzbuzz(num):
     if (int(num) % 3) == 0 and (int(num) % 5) == 0:
-        return "FizzBuzz"
+        return "Fizz Buzz"
     if (int(num) % 3) == 0:
         return "Fizz"
     if (int(num) % 5) == 0:
