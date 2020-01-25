cf5c50278dfab72059e55792433506784f2db795
dastagg(opus3) <dastagg@stagglinux.com>
2020-01-25 08:22:27 -0500

    Completed Byte 46

---
+++ 46/fizzbuzz.py
@@ -0,0 +1,8 @@
+def fizzbuzz(num):
+    if (int(value) % 3) == 0 and (int(value) % 5) == 0:
+        return "FizzBuzz"
+    if (int(value) % 3) == 0:
+        return "Fizz"
+    if (int(value) % 5) == 0:
+        return "Buzz"
+    return value
