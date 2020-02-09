6e33b290050a5432865793fe051ead039ab23778
dastagg(opus) <gitconfig>
2020-02-09 15:32:02 -0500

    Completed Bite 209

---
+++ 209/docstring.py
@@ -0,0 +1,12 @@
+def sum_numbers(numbers):
+    """Sums numbers
+
+    :param numbers : A list of numbers
+    :type list
+    ...
+    :raises TypeError: if not all numbers passed in
+    ...
+    :return: sum of numbers
+    :rtype: int
+    """
+    pass
