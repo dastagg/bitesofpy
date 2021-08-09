6d9b2339b52e1ab3edfdbb1d8812086746322640
dastagg:opus3 <dastagg@stagglinux.com>
2021-08-09 04:48:48 -0400

    Completed Bite 44

--- 44/license.py
+++ 44/license.py
@@ -1,1 +1,22 @@
-def gen_key(...
+import string
+import secrets
+
+
+def build_key_part(num_of_chars):
+    alphabet = string.ascii_uppercase + string.digits
+    password = "".join(secrets.choice(alphabet) for i in range(num_of_chars))
+    return password
+
+
+def gen_key(parts=4, chars_per_part=8):
+    license_key = ""
+    for x in range(0, parts):
+        license_key = license_key + build_key_part(chars_per_part)
+        license_key += "-"
+    # trim off the last hyphen
+    license_key = license_key[:-1]
+    return license_key
+
+
+if __name__ == "__main__":
+    print(gen_key())
