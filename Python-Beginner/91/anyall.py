73b9caa6afc36a77fc96a0f9987656e69d6c61b9
dastagg:opus5 <dastagg@stagglinux.com>
2021-10-09 19:53:00 -0400

    Completed Bite 91.

--- Python-Beginner/91/anyall.py
+++ Python-Beginner/91/anyall.py
@@ -1,3 +1,5 @@
+import re
+
 VOWELS = 'aeiou'
 PYTHON = 'python'
 
@@ -5,16 +7,17 @@ PYTHON = 'python'
 def contains_only_vowels(input_str):
     """Receives input string and checks if all chars are
        VOWELS. Match is case insensitive."""
-    pass
+    return all(c in VOWELS for c in input_str.lower())
 
 
 def contains_any_py_chars(input_str):
     """Receives input string and checks if any of the PYTHON
        chars are in it. Match is case insensitive."""
-    pass
+    # return any(c in PYTHON for c in input_str.lower())
+    return re.search(rf'[{PYTHON}]', input_str.lower())
 
 
 def contains_digits(input_str):
     """Receives input string and checks if it contains
        one or more digits."""
-    pass
+    return re.search(r'\d+', input_str)
