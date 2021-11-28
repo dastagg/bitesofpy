8e85a4aef2477b66d86a9b6c4ac30e33c9e9dd6a
dastagg:opus5 <dastagg@stagglinux.com>
2021-11-28 05:49:47 -0500

    Bite 8 - Completed.

--- 8/rotate.py
+++ 8/rotate.py
@@ -1,5 +1,8 @@
+"""Rotate the string by the number supplied."""
+
+
 def rotate(string, n):
     """Rotate characters in a string.
-       Expects string and n (int) for number of characters to move.
+    Expects string and n (int) for number of characters to move.
     """
-    pass
+    return string[n:] + string[:n]
