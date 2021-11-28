eb592a5d4bdeec08f756fe0f65860b23465c96bb
dastagg:opus5 <dastagg@stagglinux.com>
2021-11-28 06:19:03 -0500

    Bite 8 - Update with deque method.

--- 8/rotate.py
+++ 8/rotate.py
@@ -5,4 +5,18 @@ def rotate(string, n):
     """Rotate characters in a string.
     Expects string and n (int) for number of characters to move.
     """
-    return string[n:] + string[:n]
+    # Use splice method
+    # return string[n:] + string[:n]
+
+    # Use deque method
+    from collections import deque
+
+    items = deque(string)
+    # NOTE: for deque, flip the +/- sign.
+    items.rotate(-n)
+
+    # deque works in a list, convert the list to a string.
+    return "".join(items)
+
+
+print(rotate("bob and julian love pybites!", 15))
