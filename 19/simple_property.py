57ac23ec8cdd0b45865483a5766a6c7c5877f13c
dastagg:opus3 <dastagg@stagglinux.com>
2021-11-28 13:52:40 -0500

    Byte 19 - Completed.

--- 19/simple_property.py
+++ 19/simple_property.py
@@ -1,7 +1,19 @@
+"""Promo class."""
 from datetime import datetime
 
 NOW = datetime.now()
 
 
 class Promo:
-    pass
+    def __init__(self, name, expires):
+        self.name = name
+        self.expires = expires
+
+
+    @property
+    def expired(self):
+        if self.expires <= NOW:
+            return True
+        else:
+            return False
+
