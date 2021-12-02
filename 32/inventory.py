e885df15b38da66807b3341285d3875ed845e116
dastagg:opus5 <dastagg@stagglinux.com>
2021-12-01 20:33:01 -0500

    Bite 32 - Completed.

--- 32/inventory.py
+++ 32/inventory.py
@@ -1,7 +1,13 @@
-items = [{'id': 1, 'name': 'laptop', 'value': 1000},
-         {'id': 2, 'name': 'chair', 'value': 300},
-         {'id': 3, 'name': 'book', 'value': 20}]
+"""Use deepcopy to pass by value not reference."""
+
+import copy
+
+items = [
+    {"id": 1, "name": "laptop", "value": 1000},
+    {"id": 2, "name": "chair", "value": 300},
+    {"id": 3, "name": "book", "value": 20},
+]
 
 
-def duplicate_items(items):
-    return items[:]
+def duplicate_items(c_items):
+    return copy.deepcopy(c_items)
